d9c1bb82dee4205742e66accf9c2c0ec1f9a91b0
tt <1852918835@qq.com>
2024-01-08 20:45:07 +0800

    fix 0108

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '/public/style/all.css'
+import Head from 'next/head'
 function DexPert({Component}) {
     return (
-        <Component/>
+        <>
+            <Head>
+                <link rel="shortcut icon" href="/Group19.svg"/>
+                <title>DexPert</title>
+            </Head>
+            <Component/>
+        </>
     );
 }
 export default DexPert;
