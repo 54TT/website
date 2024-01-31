0611d547156eeda671d5652dba8099c3bdc94cf7
tt <1852918835@qq.com>
2024-01-31 21:55:18 +0800

    f

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
-const withTM = require('next-transpile-modules')(['somemodule', 'and-another']); // pass the modules you would like to see transpiled
-module.exports = withTM({ reactStrictMode: false,
+module.exports = {
+    reactStrictMode: false,
     compiler: {
         styledComponents: true,
-    },});
+    },
+}
