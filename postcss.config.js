69b61f1cc2478d59ae2e3c4c6af8a66a20807e8d
tt <1852918835@qq.com>
2024-01-31 22:04:00 +0800

    ffff

---
+++ postcss.config.js
@@ -0,0 +1,18 @@
+const purgecss = [
+  "@fullhuman/postcss-purgecss",
+  {
+    content: [
+      "./pages/*.js",
+      "./pages/**/*.js",
+      "./components/*.js",
+      "./components/**/*.js",
+    ],
+    whitelistPatterns: [/^slick-/],
+    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+  }
+];
+module.exports = {
+  plugins: [
+    "postcss-import",
+  ],
+};
